94c1d7c8eef2ff07554694da4095401c73745331
hillngeorge <hillngeorge@gmail.com>
2024-04-13 16:03:53 -0500

    final

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import json
 from config import dev, sum
+from data import catalog
+from flask_cors import CORS
+from flask import Flask, request
 
 
 
-app = Flask("__name__")
-
+app = Flask(__name__)
+CORS(app)  #warning, this disables cors policy
 
 @app.get("/")
 def hello():
@@ -44,6 +47,23 @@ def developer():
 def simple_sum():
     answ = sum(21, 21)
     return json.dumps(answ)
+
+
+
+@app.get("/api/products")
+def get_catlog():
+    return json.dumps(catalog)
+
+
+
+@app.post("/api/products")
+def save_products():    
+    prod = request.get_json()  #read the payload
+    catalog.append(prod)
+
+
+
+    return json.dumps(prod)
 
 
 
